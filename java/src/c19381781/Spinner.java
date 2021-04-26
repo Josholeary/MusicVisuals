48c5c9055cc191c4dbc419d0e47e858d705e5bec
Josholeary <64174317+Josholeary@users.noreply.github.com>
2021-04-26 13:17:04 +0100

    did more work on boucetube and started different styles for spinner

--- java/src/c19381781/Spinner.java
+++ java/src/c19381781/Spinner.java
@@ -12,10 +12,37 @@ public class Spinner
         this.jv = jv;
     }
 
+    float angle = 0;
+
     public void render()
     {
+        jv.stroke(255);
+        jv.translate(800, 650, -400);
+        jv.noFill();
+        jv.rotateY(angle);
+        jv.rotateZ(angle);
+        jv.rotateX(angle);
+        jv.beginShape();
+        jv.vertex(-100, -100, -100);
+        jv.vertex( 100, -100, -100);
+        jv.vertex(   0,    0,  100);
 
-    }  
-} 
+        jv.vertex( 100, -100, -100);
+        jv.vertex( 100,  100, -100);
+        jv.vertex(   0,    0,  100);
+
+        jv.vertex( 100, 100, -100);
+        jv.vertex(-100, 100, -100);
+        jv.vertex(   0,   0,  100);
+
+        jv.vertex(-100,  100, -100);
+        jv.vertex(-100, -100, -100);
+        jv.vertex(   0,    0,  100);
+        jv.endShape();
+
+        angle += jv.getSmoothedAmplitude()/15;
+         
+    } 
+}
 
            
