d0212cc2cd0befcebc92897d6d116da491e7aaf3
Josholeary <64174317+Josholeary@users.noreply.github.com>
2021-04-26 14:51:22 +0100

    Border style completed now

--- java/src/c19381781/Border.java
+++ java/src/c19381781/Border.java
@@ -6,24 +6,24 @@ import ie.tudublin.*;
 public class Border
 {
     JoshsVisual jv;
-    float x = 0;
 
     public Border(JoshsVisual jv)
     {
         this.jv = jv;
-        x = this.jv.height / 2;
     }
 
     
 
     public void render()
-    {
-        for(int i = 0 ; i < jv.getAudioBuffer().size() ; i ++)
+    {   
+        float gap = jv.width / (float) jv.getBands().length;
+        jv.noStroke();
+        for(int i = 0 ; i < jv.getBands().length ; i ++)
         {
-            jv.stroke(jv.map(i, 0, jv.getAudioBuffer().size(), 0, 255), 255, 255);
-            jv.line(i, x, i, x + x * jv.getAudioBuffer().get(i));
+            jv.fill(jv.map(jv.getSmoothedAmplitude()*2, 0, 1, 0, 255), 255, 255);
+            jv.rect(i * gap, jv.height, gap-75,-jv.getSmoothedBands()[i] * 0.15f); 
         }
-    } 
+    }
 }
 
            
