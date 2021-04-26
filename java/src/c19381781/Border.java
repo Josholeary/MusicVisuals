c9f7892cfdce6b2e288005dff8a45ed7ba73712e
Josholeary <64174317+Josholeary@users.noreply.github.com>
2021-04-26 16:04:57 +0100

    finished border for every axis

--- java/src/c19381781/Border.java
+++ java/src/c19381781/Border.java
@@ -17,12 +17,36 @@ public class Border
     public void render()
     {   
         float gap = jv.width / (float) jv.getBands().length;
+
         jv.noStroke();
         for(int i = 0 ; i < jv.getBands().length ; i ++)
         {
             jv.fill(jv.map(jv.getSmoothedAmplitude()*2, 0, 1, 0, 255), 255, 255);
-            jv.rect(i * gap, jv.height, gap-75,-jv.getSmoothedBands()[i] * 0.15f); 
+            jv.rect(i * gap, jv.height, gap-60,-jv.getSmoothedBands()[i] * 0.1f); 
         }
+
+        for(int i = 0 ; i < jv.getBands().length ; i ++)
+        {
+            jv.fill(jv.map(jv.getSmoothedAmplitude()*3, 0, 1, 0, 255), 255, 255);
+            jv.rect(jv.height+585, i*gap, -jv.getSmoothedBands()[i] * 0.1f,gap-60); 
+        }
+
+
+        for(int i = 0 ; i < jv.getBands().length ; i ++)
+        {
+            jv.fill(jv.map(jv.getSmoothedAmplitude()*4, 0, 1, 0, 255), 255, 255);
+            jv.rect(jv.height-855, i*gap, jv.getSmoothedBands()[i] * 0.1f,gap-60); 
+        }
+
+        for(int i = 0 ; i < jv.getBands().length ; i ++)
+        {
+            jv.fill(jv.map(jv.getSmoothedAmplitude()/2, 0, 1, 0, 255), 255, 255);
+            jv.rect(i * gap, jv.height-855, gap-60,jv.getSmoothedBands()[i] * 0.1f); 
+        }
+
+
+
+        
     }
 }
 
