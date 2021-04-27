8b79d878a4a23610fbae01bacb5575b569e5d450
Josholeary <64174317+Josholeary@users.noreply.github.com>
2021-04-27 23:44:52 +0100

    Updated menu positioning

--- java/src/c19381781/Menu.java
+++ java/src/c19381781/Menu.java
@@ -16,51 +16,51 @@ public class Menu
         //main headings
         jv.textSize(32);
         jv.fill(130, 255, 255);
-        jv.text("Welcome to Music Visualizer by Josh O'Leary", 400, 100);
+        jv.text("Welcome to Music Visualizer by Josh O'Leary", 375, 100);
 
         jv.fill(130, 255, 255);
-        jv.text("Student Number: C19381781", 500, 150);
+        jv.text("Student Number: C19381781", 475, 150);
 
         //music controls
         jv.fill(150, 255, 255);
-        jv.text("Music Controls:", 615, 200);
+        jv.text("Music Controls:", 590, 200);
 
         jv.textSize(24);
         jv.fill(70, 255, 255);
-        jv.text("Q - Play", 660, 250);
+        jv.text("Q - Play", 635, 250);
 
         jv.fill(0, 255, 255);
-        jv.text("W - Pause", 660, 300);
+        jv.text("W - Pause", 635, 300);
 
         jv.fill(40, 255, 255);
-        jv.text("E - Restart", 660, 350);
+        jv.text("E - Restart", 635, 350);
 
         //visual controls
         jv.textSize(32);
         jv.fill(150, 255, 255);
-        jv.text("Visual Controls:", 615, 450);
+        jv.text("Visual Controls:", 590, 450);
 
         jv.textSize(24);
         jv.fill(210, 255, 255);
-        jv.text("1 - Planets", 660, 500);
+        jv.text("1 - Planets", 635, 500);
 
         jv.fill(210, 255, 255);
-        jv.text("2 - Disco Spheres", 660, 550);
+        jv.text("2 - Disco Spheres", 635, 550);
 
         jv.fill(210, 255, 255);
-        jv.text("3 - Spinner", 660, 600);
+        jv.text("3 - Spinner", 635, 600);
 
         jv.fill(210, 255, 255);
-        jv.text("4 - Rainbow speaker", 660, 650);
+        jv.text("4 - Rainbow speaker", 635, 650);
 
         jv.fill(210, 255, 255);
-        jv.text("5 - Atom", 660, 700);
+        jv.text("5 - Atom", 635, 700);
 
         jv.fill(210, 255, 255);
-        jv.text("6 - Return to menu", 660, 750);
+        jv.text("6 - Return to menu", 635, 750);
 
         jv.fill(210, 255, 255);
-        jv.text("0 - Toggle Border", 660, 800);
+        jv.text("0 - Toggle Border", 635, 800);
 
     }
 }
